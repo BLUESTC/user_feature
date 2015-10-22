fa078e8c5a71107c2a7033da7746871ad631affd
liuyajun <liuyajun52@gmail.com>
2015-10-22 15:57:40 +0800

    初次提交

--- src/main/java/my/group/user_feature/MyMapper.java
+++ src/main/java/my/group/user_feature/MyMapper.java
@@ -2,32 +2,52 @@ package my.group.user_feature;
 
 import com.aliyun.odps.data.Record;
 import com.aliyun.odps.mapred.Mapper;
+import com.google.gson.JsonObject;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Mapper模板。请用真实逻辑替换模板内容
  */
 public class MyMapper implements Mapper {
-    private Record word;
-    private Record one;
-
+    private Record key ,value;
+    Pattern sP=Pattern.compile("(?)|(!)|(...)|(？)|(！)|(。。。)");
+    
     public void setup(TaskContext context) throws IOException {
-        word = context.createMapOutputKeyRecord();
-        one = context.createMapOutputValueRecord();
-        one.setBigint(0, 1L);
+        key = context.createMapOutputValueRecord();
+        value = context.createMapOutputValueRecord();
     }
 
     public void map(long recordNum, Record record, TaskContext context) throws IOException {
-        String uid = record.getString(2);
-        String mid=record.getString(3);
-        String action=record.getString(5);
+        String uid = record.getString(0);
+        String mid=record.getString(1);
+        String status=record.getString(3);
         
+        JsonObject json=new JsonObject();
+        json.addProperty("uid", uid);
+        json.addProperty("blog", status);
+        json.addProperty("hasTopic", status.contains("#")?1:0);
+        json.addProperty("hasUrl", status.matches(".*(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?.*")?1:0);
+        json.addProperty("hasAt", status.contains("@")?1:0);
+        json.addProperty("hasUrl", status.matches(".*\\[\\da-z+\\].*")?1:0);
+        
+
+        Matcher m=sP.matcher(status);
+        int ssSum=0;
+        while(m.find()){
+        	ssSum++;
+        }
+        json.addProperty("specialSigSum", ssSum);
+
+        
+        key.setString(0, mid);
 //        one.setBigint(0, 0L);
 //        one.setBigint(1, 0L);
 //        one.setBigint(2, 0L);
         
-        one.setBigint(0,(long) Integer.parseInt(action));
+//        one.setBigint(0,(long) Integer.parseInt(action));
 //        if(action.equals("1")){
 //        	one.setBigint(0, 1L);
 //        }
@@ -40,9 +60,9 @@ public class MyMapper implements Mapper {
 //       
 //        
 //        
-        word.setString(0, mid);
-
-        context.write(word, one);
+//        word.setString(0, mid);
+        value.setString(0, json.toString());
+        context.write(key, value);
     }
 
     public void cleanup(TaskContext context) throws IOException {
--- src/main/java/my/group/user_feature/MyReducer.java
+++ src/main/java/my/group/user_feature/MyReducer.java
@@ -18,24 +18,27 @@ public class MyReducer implements Reducer {
     }
 
     public void reduce(Record key, Iterator<Record> values, TaskContext context) throws IOException {
-        long zhuan = 0,ping=0,zan=0;
-        while (values.hasNext()) {
-            Record val = values.next();
-            if(val.getBigint(0)==0)
-            zhuan += 1;
-            if(val.getBigint(0)==1)
-            ping += 1;
-            if(val.getBigint(0)==2)
-            zan += 1;
-        }
-//        HashMap<String,Long> map=new HashMap<String, Long>();
-//        map.put("repost", zhuan);
-//        map.put("comment", ping);
-//        map.put("like", zan);
-        result.set(0, key.getString(0));
-        result.set(1, "{repost:"+zhuan+",comment:"+ping+",like:"+zan+"}");
-//        result.setBigint(0, zhuan);
-        context.write(result);
+    	
+//        long zhuan = 0,ping=0,zan=0;
+//        while (values.hasNext()) {
+//            Record val = values.next();
+//            if(val.getBigint(0)==0)
+//            zhuan += 1;
+//            if(val.getBigint(0)==1)
+//            ping += 1;
+//            if(val.getBigint(0)==2)
+//            zan += 1;
+//        }
+////        HashMap<String,Long> map=new HashMap<String, Long>();
+////        map.put("repost", zhuan);
+////        map.put("comment", ping);
+////        map.put("like", zan);
+    	while(values.hasNext()){
+            result.set(0, key.getString(0));
+            result.set(1, values.next().getString(0));
+            context.write(result);
+    	}
+
     }
 
     public void cleanup(TaskContext arg0) throws IOException {
